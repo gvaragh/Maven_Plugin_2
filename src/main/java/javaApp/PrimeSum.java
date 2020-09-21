0c1b25a340119b58144245844d93f6a94902ca09
Vasu <gvaragh@gmail.com>
2020-09-21 19:16:10 +0800

    Modified files

--- src/main/java/javaApp/PrimeSum.java
+++ src/main/java/javaApp/PrimeSum.java
@@ -9,8 +9,29 @@ public class PrimeSum {
     static long primesum(int maxNum) {
         int sum = 0, num;
         //insert your code here
-
-
+        if(maxNum < 0 || maxNum ==0)
+        {
+        	return 0;
+        }
+        for(int i=2;i<=maxNum; i++)
+        {
+        	boolean bool = isPrime(i);
+        	if(bool) {
+        		sum = sum +i;
+        	}
+        }
         return sum;
+    }
+    
+    static boolean isPrime(int num) {
+    	int sqrt = (int) Math.sqrt(num)+1;
+    	for ( int i= 2; i< sqrt; i++)
+    	{
+    		if(num % i == 0 )
+    		{
+    			return false;
+    		}
+    	}
+    	return true;
     }
 } 
